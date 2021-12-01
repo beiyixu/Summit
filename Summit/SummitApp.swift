bda455d7a63c9199d003d1f4bfa722e9aff4c40d
Beiyi Xu <beiyixu2@gmail.com>
2021-11-30 21:07:48 -0700

    Initial Commit

---
+++ Summit/SummitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummitApp.swift
+//  Summit
+//
+//  Created by Beiyi Xu on 11/30/21.
+//
+
+import SwiftUI
+
+@main
+struct SummitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
